2ef68cdc47c9e78148e67549b9537faa0043c252
Ruben Esteche <rubenesteche@hotmail.com>
2022-06-27 20:06:49 -0300

    Add files via upload

--- ML_projects/seoul_regression/bikes_ML.py
+++ ML_projects/seoul_regression/bikes_ML.py
@@ -9,13 +9,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #exporting and understanding our data
-
 data = pd.read_csv('SeoulBikeData.csv', encoding= 'unicode_escape')
+print('Our data labeled in a table format:')
 print(data.head())
+
+print('Relevant statistical quantities')
+print(data.describe)
+
+print('Relevant computation quantities')
+print (data.dtypes)
+print (data.info())
+
 
 #labeling our data
 #X stands for data/ y stands for target 
-
 X = data[['Hour',
           'Temperature(°C)',
           'Rainfall(mm)'
@@ -28,7 +35,6 @@ print("X_shape: ", X.shape)
 print("y_shape: ", y.shape)
 
 #visualizing the data 
-
 plt.scatter(X.T[0], y, s = 4, label='hours')
 plt.scatter(X.T[1], y, s = 4,label='temperature')
 plt.scatter(X.T[2], y, s = 4,label='rainfall')
@@ -36,7 +42,6 @@ plt.legend()
 plt.show()
 
 #separating in three plots for better understanding the units
-
 fig, ax = plt.subplots(1, 3, sharex=False, sharey=False, figsize=(17, 6)) 
 
 ax[0].scatter(X.T[0], y, color='r', s = 4)
@@ -57,12 +62,10 @@ ax[2].set_xlabel('X label features')
 plt.show()
 
 #Let's begin the machine learning exploration!
-
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split 
 
 #splitting the data 
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
 plt.scatter(X_test.T[1], y_test, s = 4, label = 'data points')
@@ -70,7 +73,6 @@ plt.title('Target points for model testing')
 plt.show()
 
 #model / algorithm and training - LINEAR REGRESSION
-
 l_reg = linear_model.LinearRegression()
 model  = l_reg.fit(X_train, y_train)
 predictions = model.predict(X_test)
@@ -81,7 +83,6 @@ print('intercept: ', l_reg.intercept_)
 
 #identificamos overfitting mesmo realizando o split de teste.
 #R^2 aprox accuracy na regressão linear, muito falha no teste de cross-validation
- 
 plt.scatter(X_test.T[1], y_test, s = 4, label = 'data points')
 plt.scatter(X_test.T[1], predictions, color="black", s = 4, label = 'model predictions')
 plt.title('Model predictions under linear regression regime')
