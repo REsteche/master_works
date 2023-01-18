adaaec9e871e904e93931943219cbbb6fc27f57f
REsteche <rubenesteche@hotmail.com>
2023-01-18 15:51:39 -0300

    adding heatmap for feature selection

--- ML_projects/seoul_regression/bikes_ML.py
+++ ML_projects/seoul_regression/bikes_ML.py
@@ -6,23 +6,35 @@ Created on Wed Jun 22 17:58:12 2022
 """
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
-#exporting and understanding our data
+# exporting and understanding our data
 data = pd.read_csv('SeoulBikeData.csv', encoding= 'unicode_escape')
-print('Our data labeled in a table format:')
+print('\nOur data labeled in a table format:')
 print(data.head())
 
-print('Relevant statistical quantities')
+print('\nRelevant statistical quantities')
 print(data.describe)
 
-print('Relevant computation quantities')
+print('\nRelevant computation quantities')
 print (data.dtypes)
 print (data.info())
 
+# removing string from our relational dataframe to future analisys
+data.drop(['Date', 'Seasons', 'Holiday', 'Functioning Day'], axis=1, inplace=True)
+print('\nOur data without string columns:')
+print(data.head())
 
-#labeling our data
-#X stands for data/ y stands for target 
+# Heatmap visualization
+print('\nHeatmap to visualize the correlation between the features')
+corr = data.corr()
+ax = sns.heatmap(corr, annot=True)
+plt.show()
+
+
+# labeling our data
+# X stands for data/ y stands for target 
 X = data[['Hour',
           'Temperature(°C)',
           'Rainfall(mm)'
@@ -34,14 +46,14 @@ print(X,y)
 print("X_shape: ", X.shape)
 print("y_shape: ", y.shape)
 
-#visualizing the data 
+# visualizing the data 
 plt.scatter(X.T[0], y, s = 4, label='hours')
 plt.scatter(X.T[1], y, s = 4,label='temperature')
 plt.scatter(X.T[2], y, s = 4,label='rainfall')
 plt.legend()
 plt.show()
 
-#separating in three plots for better understanding the units
+# separating in three plots for better understanding the units
 fig, ax = plt.subplots(1, 3, sharex=False, sharey=False, figsize=(17, 6)) 
 
 ax[0].scatter(X.T[0], y, color='r', s = 4)
@@ -61,18 +73,18 @@ ax[2].set_xlabel('X label features')
 
 plt.show()
 
-#Let's begin the machine learning exploration!
+# Let's begin the machine learning exploration!
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split 
 
-#splitting the data 
+# splitting the data 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
 plt.scatter(X_test.T[1], y_test, s = 4, label = 'data points')
 plt.title('Target points for model testing')
 plt.show()
 
-#model / algorithm and training - LINEAR REGRESSION
+# model / algorithm and training - LINEAR REGRESSION
 l_reg = linear_model.LinearRegression()
 model  = l_reg.fit(X_train, y_train)
 predictions = model.predict(X_test)
@@ -81,8 +93,8 @@ print('R^2 value: ', l_reg.score(X,y))
 print('coefficient factor: ', l_reg.coef_)
 print('intercept: ', l_reg.intercept_)
 
-#identificamos overfitting mesmo realizando o split de teste.
-#R^2 aprox accuracy na regressão linear, muito falha no teste de cross-validation
+# Overfitting even without test split.
+# R^2 aprox accuracy for linear reg, huge fail in the cross-validation test
 plt.scatter(X_test.T[1], y_test, s = 4, label = 'data points')
 plt.scatter(X_test.T[1], predictions, color="black", s = 4, label = 'model predictions')
 plt.title('Model predictions under linear regression regime')
@@ -92,7 +104,7 @@ plt.ylim([0, 3000])
 plt.legend()
 plt.show()
 
-#model / algorithm and training - KNN REGRESSION
+# model / algorithm and training - KNN REGRESSION
 from sklearn import neighbors
 
 knn = neighbors.KNeighborsRegressor(n_neighbors= 5, weights= 'uniform')
@@ -116,7 +128,7 @@ plt.ylim([0, 3000])
 plt.legend()
 plt.show()
 
-#model / algorithm and training - SVM REGRESSION
+# model / algorithm and training - SVM REGRESSION
 from sklearn import svm
 
 model =  svm.SVR(kernel='poly')
